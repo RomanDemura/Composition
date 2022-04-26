4560fabf483d868d9d0fd59e023479117e741149
Roman Demura <anokhin.roman.27@gmail.com>
2022-04-26 18:55:47 +0300

    Add viewBinding

--- app/src/main/java/tech/demura/composition/presentation/GameFragment.kt
+++ app/src/main/java/tech/demura/composition/presentation/GameFragment.kt
@@ -6,14 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import tech.demura.composition.R
+import tech.demura.composition.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private var _bindind: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+    get() = _bindind ?: throw RuntimeException("FragmentGameBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        _bindind = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _bindind = null
     }
 
 }
