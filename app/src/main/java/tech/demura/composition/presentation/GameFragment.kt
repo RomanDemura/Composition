eba920c848ed3e5dc0cb1ee3afc9ed48040986aa
Roman Demura <anokhin.roman.27@gmail.com>
2022-04-26 21:31:48 +0300

    Add fragment navigation

--- app/src/main/java/tech/demura/composition/presentation/GameFragment.kt
+++ app/src/main/java/tech/demura/composition/presentation/GameFragment.kt
@@ -7,12 +7,24 @@ import android.view.View
 import android.view.ViewGroup
 import tech.demura.composition.R
 import tech.demura.composition.databinding.FragmentGameBinding
+import tech.demura.composition.domain.entity.GameResult
+import tech.demura.composition.domain.entity.GameSettings
+import tech.demura.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
 
     private var _bindind: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
-    get() = _bindind ?: throw RuntimeException("FragmentGameBinding == null")
+        get() = _bindind ?: throw RuntimeException("FragmentGameBinding == null")
+
+    private lateinit var level: Level
+    private var gameSettings = GameSettings(22,2,4,5)
+    private var gameResult = GameResult(true,1,2, gameSettings)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArguments()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,9 +34,46 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.option1.setOnClickListener {
+            launchGameFinishedFragment(gameResult)
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _bindind = null
+    }
+
+    private fun parseArguments() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    private fun launchGameFinishedFragment(
+        gameResult: GameResult
+    ) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(
+                R.id.main_container, GameFinishedFragment.newInstance(
+                    gameResult
+                )
+            )
+            .addToBackStack(GameFinishedFragment.GAME_FINISHED_FRAGMENT)
+            .commit()
+    }
+
+    companion object {
+        const val GAME_FRAGMENT = "game_fragment"
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/tech/demura/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/tech/demura/composition/presentation/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import tech.demura.composition.R
+import tech.demura.composition.databinding.FragmentChooseLevelBinding
 import tech.demura.composition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment: Fragment() {
@@ -23,9 +25,25 @@ class WelcomeFragment: Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener{
+            launchChooseLevelFragment()
+        }
+    }
+
+    fun launchChooseLevelFragment(){
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, ChooseLevelFragment.newInstance())
+            .addToBackStack(ChooseLevelFragment.CHOOSE_LEVEL_FRAGMENT)
+            .commit()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 
 }
